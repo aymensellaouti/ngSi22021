ff75cf2707bb3d4d845a07857ae638d4349e58b3
aymen2782 <aymen.sellaouti@gmail.com>
2021-02-17 10:12:51 +0100

    two way

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,23 @@ import { AppComponent } from './app.component';
 import { FirstComponent } from './first/first.component';
 import { SecondComponent } from './second/second.component';
 import { ThirdComponent } from './third/third.component';
+import { ColorComponent } from './components/color/color.component';
+import { FormsModule } from '@angular/forms';
+import { TwoComponent } from './components/two/two.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstComponent,
     SecondComponent,
-    ThirdComponent
+    ThirdComponent,
+    ColorComponent,
+    TwoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
