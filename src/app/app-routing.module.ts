afc4bf96076e7fe32a29ebe4b2f9b1ecea13f296
aymen2782 <aymen.sellaouti@gmail.com>
2021-04-14 12:57:46 +0100

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CvComponent } from './cv/cv/cv.component';
+import { TodoComponent } from './todo/todo/todo.component';
+import { ColorComponent } from './components/color/color.component';
+import { MiniWordComponent } from './directives/mini-word/mini-word.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'cv', component: CvComponent},
+  {path: 'todo', component: TodoComponent},
+  {path: 'color', component: ColorComponent},
+  {path: 'word', component: MiniWordComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
