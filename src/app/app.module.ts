edbc91c6697860d294b1f2e25d290cb46a55c5b5
aymen2782 <aymen.sellaouti@gmail.com>
2021-05-05 15:43:48 +0100

    traduction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -44,6 +44,8 @@ import { TestFormComponent } from './components/test-form/test-form.component';
 import { TestObservableComponent } from './components/test-observable/test-observable.component';
 import { SliderComponent } from './components/slider/slider.component';
 import { AUTH_INTERCEPTOR } from './interceptors/auth.interceptor';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 
 @NgModule({
@@ -90,9 +92,21 @@ import { AUTH_INTERCEPTOR } from './interceptors/auth.interceptor';
     FormsModule,
     BrowserAnimationsModule, // required animations module
     ToastrModule.forRoot(),
-    HttpClientModule
+    HttpClientModule,
+    TranslateModule.forRoot({
+      defaultLanguage: 'fr',
+      loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [HttpClient],
+              }
+    })
   ],
   providers: [AUTH_INTERCEPTOR],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export function HttpLoaderFactory(http: HttpClient) {
+    return new TranslateHttpLoader(http, 'assets/i18n/', '.json');
+  }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,6 +3,7 @@ import { AuthService } from '../../auth/services/auth.service';
 import { Router } from '@angular/router';
 import { routes } from '../../constantes/routres';
 import { ToastrService } from 'ngx-toastr';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
@@ -13,13 +14,23 @@ export class HeaderComponent implements OnInit {
   constructor(
     public authService: AuthService,
     public router: Router,
-    private toaster: ToastrService
-  ) {}
+    private toaster: ToastrService,
+    public translateService: TranslateService
+  ) {
+      translateService.setDefaultLang('fr');
+      translateService.addLangs(['en', 'fr', 'ar']);
+      const browserLang = translateService.getBrowserLang();
+      translateService.use(browserLang.match(/en|fr|ar/) ? browserLang : translateService.getDefaultLang());
+  }
 
   ngOnInit(): void {}
   logout(): void {
     this.authService.logout();
     this.toaster.info('A la prochaine fois :)');
     this.router.navigate([routes.login]);
+  }
+
+  translate(langage) {
+    this.translateService.use(langage);
   }
 }
